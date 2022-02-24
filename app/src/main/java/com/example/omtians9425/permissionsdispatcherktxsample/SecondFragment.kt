5197c3e569c3393584f6924baa0ceb1a2e57d0bf
omtians9425 <konosuke.orii@quipper.com>
2022-02-25 01:24:38 +0900

    Request permission

--- app/src/main/java/com/example/omtians9425/permissionsdispatcherktxsample/SecondFragment.kt
+++ app/src/main/java/com/example/omtians9425/permissionsdispatcherktxsample/SecondFragment.kt
@@ -1,5 +1,36 @@
 package com.example.omtians9425.permissionsdispatcherktxsample
 
+import android.Manifest
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+import permissions.dispatcher.ktx.PermissionsRequester
+import permissions.dispatcher.ktx.constructPermissionsRequest
 
-class SecondFragment : Fragment(R.layout.fragment_second)
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+    private lateinit var permissionsRequester: PermissionsRequester
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        permissionsRequester = constructPermissionsRequest(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            onShowRationale = null,
+            onNeverAskAgain = null,
+            onPermissionDenied = { Toast.makeText(requireContext(), "Denied.", Toast.LENGTH_SHORT).show() }
+        ) {
+            Toast.makeText(requireContext(), "Granted.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireView().findViewById<MaterialButton>(R.id.button).setOnClickListener {
+            permissionsRequester.launch()
+        }
+    }
+}
