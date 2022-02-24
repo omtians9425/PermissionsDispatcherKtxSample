2bc877eb6fb3a0b0c40734835d455621e1015144
omtians9425 <konosuke.orii@quipper.com>
2022-02-25 00:40:37 +0900

    Initial commit

---
+++ app/src/main/java/com/example/omtians9425/permissionsdispatcherktxsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.omtians9425.permissionsdispatcherktxsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
